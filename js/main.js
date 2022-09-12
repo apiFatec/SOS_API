14044e38e122357f2d46baa1ae770fe26637c76e
Caboia <caio.rodriguesyokoyama@gmail.com>
2022-09-12 16:21:17 -0300

    :sparkles: feat: Navbar layout, numeração pcs

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// var dropSala = window.document.getElementById("sala");
+// var dropAndar = window.document.getElementById("andar");
+
+// dropSala.addEventListener("click", clickSala);
+// dropAndar.addEventListener("click", clickAndar);
+
+// function clickSala() {
+//   var elementoSala1 = document.createElement("li");
+//   var elementoSala2 = document.createElement("li");
+//   var elementoSala3 = document.createElement("li");
+//   var elementoSala4 = document.createElement("li");
+//   var elementoSala5 = document.createElement("li");
+//   var elementoSala6 = document.createElement("li");
+//   var elementoSala7 = document.createElement("li");
+//   var elementoSala8 = document.createElement("li");
+//   elementoSala1.textContent = "Sala 402";
+//   elementoSala2.textContent = "Sala 403";
+//   elementoSala3.textContent = "Sala 404";
+//   elementoSala4.textContent = "Sala 405";
+//   elementoSala5.textContent = "Sala 406";
+//   elementoSala6.textContent = "Sala 407";
+//   elementoSala7.textContent = "Sala 408";
+//   elementoSala8.textContent = "Sala 409";
+
+//   dropSala
+//     .appendChild(elementoSala1)
+//     .appendChild(elementoSala2)
+//     .appendChild(elementoSala3)
+//     .appendChild(elementoSala4)
+//     .appendChild(elementoSala5)
+//     .appendChild(elementoSala6)
+//     .appendChild(elementoSala7)
+//     .appendChild(elementoSala8);
+
+//   dropSala.addEventListener("click", fechar);
+//   function fechar() {
+//     dropSala.removeChild(dropSala.lastElementChild);
+//   }
+// }
+
+// function clickAndar() {
+//   var elementoTerreo = document.createElement("li");
+//   var elementoAndar2 = document.createElement("li");
+//   var elementoAndar3 = document.createElement("li");
+//   var elementoAndar4 = document.createElement("li");
+//   var elementoAndar5 = document.createElement("li");
+//   elementoTerreo.textContent = "Térreo";
+//   elementoAndar2.textContent = "Andar 1";
+//   elementoAndar3.textContent = "Andar 2";
+//   elementoAndar4.textContent = "Andar 3";
+//   elementoAndar5.textContent = "Andar 4";
+
+//   dropAndar
+//     .appendChild(elementoTerreo)
+//     .appendChild(elementoAndar2)
+//     .appendChild(elementoAndar3)
+//     .appendChild(elementoAndar4)
+//     .appendChild(elementoAndar5);
+// }
