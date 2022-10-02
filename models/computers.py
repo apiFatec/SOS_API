232ac884a1a3cf7084a9c737d13116525047230a
imbard <nicota1234@gmail.com>
2022-10-02 01:11:41 -0300

    :sparkles: feat: Class Computers to manipulate

---
+++ models/computers.py
@@ -0,0 +1,17 @@
+from app import db
+import enum
+
+class SisType(enum.Enum):
+  old = "32"
+  new = "64"
+
+class Computers(db.Model):
+  id = db.Column(db.Integer, primary_key=True, nullable=False)
+  Processador = db.Column(db.String, nullable=False)
+  ram = db.Column(db.String, nullable=False)
+  tipo_sis = db.Column(enum.Enum(SisType), nullable=False)
+  Win = db.Column(db.String, nullable=False)
+  Versao = db.Column(db.String, nullable=False)
+  idEndereco = db.Comlumn(db.Integer, db.ForeignKey("address.id"), nullable=False)
+
+
