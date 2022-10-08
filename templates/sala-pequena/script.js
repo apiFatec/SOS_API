79cbf417869f67c18a000f15bddab276049c2d7a
imbard <nicota1234@gmail.com>
2022-10-08 20:50:54 -0300

    :fire: Old scripts.js

--- templates/sala-pequena/script.js
+++
@@ -1,9 +0,0 @@
-const queryString = window.location.search
-const urlParams = new URLSearchParams(queryString)
-const sala = urlParams.get("sala")
-const piso = urlParams.get("piso")
-window.onload = () => {
-  let a = document.getElementById("avoltar")
-  a.setAttribute("href", `../pisos/index.html?piso=${piso}`)
-}
-
