37cfdac181d83f9014bd6065fbf64fda74059eb8
imbard <nicota1234@gmail.com>
2022-10-11 01:14:40 -0300

    :sparkles: feat: Session init

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from flask import Flask
 from routes import main
+from flask_session import Session
 from db import db
 import os
 
@@ -8,9 +9,12 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 db.init_app(app)
 
 app.register_blueprint(main)
 
 if __name__ == "__main__": 
-  app.run()
+  app.run(debug=True)
