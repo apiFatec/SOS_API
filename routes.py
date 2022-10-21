4cabf9664a5bb13af458c18a529fb94c7df6a66a
imbard <nicota1234@gmail.com>
2022-10-21 15:36:42 -0300

    :recycle: refactor: Retira print e espaços

--- routes.py
+++ routes.py
@@ -143,9 +143,7 @@ def HomeChamado():
 
 @main.route('/<int:sala>/<int:num>/teladetalhes')
 def teladetalhes(sala,num):
-
   comp =  Computadores.query.filter_by(sala=sala, numero=num).first()
-  print(comp)
   return render_template('tela-detalhes/index.html', comp=comp)
 
 def insertDate():
