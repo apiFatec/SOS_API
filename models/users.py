088b58407213f5cb51e276f4390d7a0d00101b18
imbard <nicota1234@gmail.com>
2022-11-03 17:10:17 -0300

    :construction: Add field confirmed in Users

--- models/users.py
+++ models/users.py
@@ -6,5 +6,6 @@ class Users(db.Model):
   id_usuario = db.Column(db.Integer, primary_key=True, nullable=False)
   nome = db.Column(db.String, nullable=False)
   senha = db.Column(db.String(72), nullable=False)
-  email = db.Column(db.String, nullable=False)
+  email = db.Column(db.String(120), nullable=False, unique=True)
   turma = db.Column(db.String, nullable=True)
+  confirmed = db.Column(db.Boolean, default=False, nullable=True)
