76db3689fa01baa0fdaef8594eca5114aafdf1be
imbard <nicota1234@gmail.com>
2022-11-05 00:33:38 -0300

    :construction: Adiciona chave estrangeira tbl_chamados

--- models/tbl_chamados.py
+++ models/tbl_chamados.py
@@ -1,6 +1,7 @@
 import email
 from db import db
 from models.tbl_computador import Computador
+from models.users import Users
 
 class TblChamados(db.Model):
   __tablename__ = "tbl_chamados"
@@ -15,3 +16,4 @@ class TblChamados(db.Model):
   email = db.Column(db.String, nullable=True)
   data = db.Column(db.String, nullable=False)
   fk_idComputador = db.Column(db.Integer, db.ForeignKey(Computador.idComputador))
+  fk_idUsuario = db.Column(db.Integer, db.ForeignKey(Users.id_usuario), default=0)
--- models/users.py
+++ models/users.py
@@ -5,7 +5,9 @@ class Users(db.Model):
 
   id_usuario = db.Column(db.Integer, primary_key=True, nullable=False)
   nome = db.Column(db.String, nullable=False)
-  senha = db.Column(db.String(72), nullable=False)
+  senha = db.Column(db.String(255), nullable=False)
   email = db.Column(db.String(120), nullable=False, unique=True)
   turma = db.Column(db.String, nullable=True)
   confirmed = db.Column(db.Boolean, default=False, nullable=True)
+  count_chamados = db.Column(db.Integer, nullable=True)
+  chamados = db.relationship("TblChamados", backref='users')
