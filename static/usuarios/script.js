528fd62d9893da636a2bdc26b22c1d0dd0b6e02b
imbard <nicota1234@gmail.com>
2022-11-05 00:30:00 -0300

    :sparkles: feat: Implementa flask e javascript na tela de usuarios

---
+++ static/usuarios/script.js
@@ -0,0 +1,25 @@
+function SubmitForm() {
+  document.formulario.submit()
+}
+
+function edit(event) {
+  event.preventDefault()
+  nome = document.getElementById('name')
+  turma = document.getElementById('turma')
+
+  nome.style.border = "1px solid black"
+  turma.style.border = "1px solid black"
+  nome.style.borderRadius = "5px"
+  turma.style.borderRadius = "5px"
+  
+  nome.style.padding = "5px"
+  turma.style.padding = "5px"
+
+
+
+  nome.disabled = false
+  turma.disabled = false
+
+  console.log(nome.disabled, turma.disabled)
+
+}
